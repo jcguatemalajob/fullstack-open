19869bb48c81978646094853a34bf6921cb8e46d
dataumgy@dataelectric.tech <dataumgy@dataelectric.tech>
2025-08-15 06:32:54 +0100

    modified:   part1/cComponent/src/App.jsx

--- part1/cComponent/src/App.jsx
+++ part1/cComponent/src/App.jsx
@@ -1,61 +1,24 @@
-import { useState } from 'react'
-
-//Using props
 const Hello = (props) => {
-  const name = props.name
-  const age = props.age
-  //Component helper function
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
-
   return (
     <div>
-      <p>Hello, {name}, you are {age} years old</p>
-      <p>You were probably born in {bornYear()}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-//Using props destructuring
-const Hello2 = ({ name, age }) => {
-  return (
-    <div>
-      <p>Hello, {name}, you are {age} years old</p>
-    </div>
-  )
-}
+const App = () => {
+  const name = 'Peter'
+  const age = 10
 
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-      
-//     </>
-//   )
-// }
-//Adding the props
-const App = (props) => {
-
-  const { counter } = props
-
-  
-  const name = 'Amarok'
-  const age = 10000
   return (
     <div>
       <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Hello name="Inti" age={100} />
-      <Hello2 name="Inti" age={100} />
-      <p>Counter: {counter}</p>
     </div>
   )
 }
-
 
 export default App
