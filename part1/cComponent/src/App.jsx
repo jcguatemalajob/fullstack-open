ff9402d9b65a5842916101511ac4a581c3a7b29e
dataumgy@dataelectric.tech <dataumgy@dataelectric.tech>
2025-08-15 06:32:27 +0100

    modified:   part1/cComponent/src/App.jsx 	modified:   part1/cComponent/src/main.jsx 	modified:   part1/cComponent/src/App.jsx

--- part1/cComponent/src/App.jsx
+++ part1/cComponent/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+//Using props
 const Hello = (props) => {
   const name = props.name
   const age = props.age
@@ -17,6 +18,15 @@ const Hello = (props) => {
   )
 }
 
+//Using props destructuring
+const Hello2 = ({ name, age }) => {
+  return (
+    <div>
+      <p>Hello, {name}, you are {age} years old</p>
+    </div>
+  )
+}
+
 
 
 // function App() {
@@ -28,8 +38,12 @@ const Hello = (props) => {
 //     </>
 //   )
 // }
+//Adding the props
+const App = (props) => {
 
-const App = () => {
+  const { counter } = props
+
+  
   const name = 'Amarok'
   const age = 10000
   return (
@@ -37,6 +51,8 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name={name} age={age} />
       <Hello name="Inti" age={100} />
+      <Hello2 name="Inti" age={100} />
+      <p>Counter: {counter}</p>
     </div>
   )
 }
